60f68d0daf7c8e8d60a761f73fc505dbca42cf78
Andrew <nyandrewdesu@live.com>
2024-10-18 03:28:06 -0700

    first commit

--- bin/cdk-keycloak-template.ts
+++ bin/cdk-keycloak-template.ts
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkKeycloakTemplateStack } from '../lib/cdk-keycloak-template-stack';
+import "dotenv/config";
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkKeycloakTemplateStack } from "../lib/cdk-keycloak-template-stack";
+
+const {
+  CDK_DEFAULT_ACCOUNT = "",
+  CDK_DEFAULT_REGION = "",
+  AWS_ACCOUNT = CDK_DEFAULT_ACCOUNT,
+  AWS_REGION = CDK_DEFAULT_REGION,
+} = process.env;
 
 const app = new cdk.App();
-new CdkKeycloakTemplateStack(app, 'CdkKeycloakTemplateStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+console.log({ account: AWS_ACCOUNT, region: AWS_REGION });
+new CdkKeycloakTemplateStack(app, "CdkKeycloakTemplateStack", {
+  env: { account: AWS_ACCOUNT, region: AWS_REGION },
 });
--- lib/cdk-keycloak-template-stack.ts
+++ lib/cdk-keycloak-template-stack.ts
@@ -1,16 +1,31 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import "dotenv/config";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecsp from "aws-cdk-lib/aws-ecs-patterns";
+
+const { KC_BOOTSTRAP_ADMIN_USERNAME = "", KC_BOOTSTRAP_ADMIN_PASSWORD = "" } =
+  process.env;
+const keycloakImage = "quay.io/keycloak/keycloak";
+const keycloakTag = "26.0";
 
 export class CdkKeycloakTemplateStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkKeycloakTemplateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ecsp.ApplicationLoadBalancedFargateService(this, "Keycloak", {
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry(
+          `${keycloakImage}${keycloakTag}`
+        ),
+        containerPort: 8080,
+        command: ["start-dev"],
+        environment: {
+          KC_BOOTSTRAP_ADMIN_USERNAME,
+          KC_BOOTSTRAP_ADMIN_PASSWORD,
+        },
+      },
+      publicLoadBalancer: true,
+    });
   }
 }
